486976ba6be2b55dfc1ca913098b61858df09c59
DjMagic32 <garciawork3232@gmail.com>
2024-04-12 12:18:26 -0400

    fixes styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,7 +81,7 @@ export default function Home() {
   return (
 
     <FormProvider  {...methods}>
-      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <main className="flex bg-slate-100 min-h-screen flex-col items-center justify-center p-3 sm:p-24">
 
         {loading && (
           <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
@@ -91,20 +91,20 @@ export default function Home() {
 
         {status === "form" &&
           <div className='flex justify-center items-center flex-col mb-5'>
-            <h1 className='text-2xl font-bold'>Share Your Opinion</h1>
-            <p className='text-red-600'>Your opinion will remain anonymous, and we will not share your information.</p>
+            <h1 className='text-2xl font-bold text-center'>Share Your Opinion</h1>
+            <p className='text-red-600 text-center'>Your opinion will remain anonymous, and we will not share your information.</p>
           </div>
         }
 
         {status === "success" &&
           <div className='flex justify-center items-center flex-col'>
-            <h2 className='text-2xl font-bold'>Thank you very much for collaborating with us and completing the survey. Your participation is greatly appreciated!</h2>
+            <h2 className='text-2xl font-bold text-center'>Thank you very much for collaborating with us and completing the survey. Your participation is greatly appreciated!</h2>
             <svg xmlns="http://www.w3.org/2000/svg" height="300" viewBox="0 -960 960 960" fill='green' ><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" /></svg>
           </div>
         }
 
         {status === "form" &&
-          <form onSubmit={methods.handleSubmit(handleFormSubmit)}>
+          <form className='w-[100%] flex justify-center items-center flex-col' onSubmit={methods.handleSubmit(handleFormSubmit)}>
             {step === 1 && (
               <motion.div
                 key="voterInfo"
@@ -113,7 +113,7 @@ export default function Home() {
                 exit="out"
                 variants={pageVariants}
                 transition={pageTransition}
-                className='bg-blue-300 p-10 w-[700px] h-[500px] gap-5 flex flex-col justify-between rounded-[10px]'
+                className='bg-blue-300 p-10 w-[100%] sm:w-[550px] h-[550px] gap-5 flex flex-col justify-between rounded-[10px]'
               >
                 <VoterInfo />
               </motion.div>
@@ -126,7 +126,7 @@ export default function Home() {
                 exit="out"
                 variants={pageVariants}
                 transition={pageTransition}
-                className='bg-blue-300 p-10 w-[700px] h-[500px] gap-5 flex flex-col justify-between rounded-[10px]'
+                className='bg-blue-300 p-10 w-[100%] sm:w-[550px] h-[550px] gap-5 flex flex-col justify-between rounded-[10px]'
               >
                 <Verification />
               </motion.div>
@@ -139,7 +139,7 @@ export default function Home() {
                 exit="out"
                 variants={pageVariants}
                 transition={pageTransition}
-                className='bg-blue-300 p-10 w-[700px] h-[500px] gap-5 flex flex-col justify-between rounded-[10px]'
+                className='bg-blue-300 p-10 sm:w-[550px] w-[100%] h-[550px] gap-5 flex flex-col justify-between rounded-[10px]'
               >
                 <VotingPreferences />
               </motion.div>
@@ -152,7 +152,7 @@ export default function Home() {
                 exit="out"
                 variants={pageVariants}
                 transition={pageTransition}
-                className='bg-blue-300 p-10 w-[700px] h-[500px] gap-5 flex flex-col justify-between rounded-[10px]'
+                className='bg-blue-300 p-10 w-[100%] sm:w-[550px] h-[550px] gap-5 flex flex-col justify-between rounded-[10px]'
               >
                 <OpinionQuestions />
               </motion.div>
@@ -165,7 +165,7 @@ export default function Home() {
                 exit="out"
                 variants={pageVariants}
                 transition={pageTransition}
-                className='bg-blue-300 p-10 w-[700px] h-[500px] gap-5 flex flex-col justify-between rounded-[10px]'
+                className='bg-blue-300 p-10 w-[100%] sm:w-[550px] h-[550px] gap-5 flex flex-col justify-between rounded-[10px]'
               >
                 <ReviewAndSubmit />
               </motion.div>
